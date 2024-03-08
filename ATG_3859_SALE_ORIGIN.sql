40e961b2587b118ccc6595042acdb394ee18850a
Service Web <sw@gssmail.com>
2024-03-08 17:18:57 -0600

    Commit changes in C:\Websites\Git\emarlow\3859\ATG_3859_SALE_ORIGIN.sql

---
+++ ATG_3859_SALE_ORIGIN.sql
@@ -0,0 +1,7 @@
+/* ==========================================
+ * TABLE: ATG_3859_SALE_ORIGIN
+ * ========================================== */
+CREATE TABLE "ATG_3859_SALE_ORIGIN"(
+ "ID" IDENTITY DEFAULT '0',
+ "DESCRIPTION" CHAR(30) NOT NULL ,
+ UNIQUE ("ID"));
