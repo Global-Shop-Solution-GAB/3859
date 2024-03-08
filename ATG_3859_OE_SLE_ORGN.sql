f58a6095b47333588943986c3163412b35f4b669
Service Web <sw@gssmail.com>
2024-03-08 17:18:57 -0600

    Commit changes in C:\Websites\Git\emarlow\3859\ATG_3859_OE_SLE_ORGN.sql

---
+++ ATG_3859_OE_SLE_ORGN.sql
@@ -0,0 +1,7 @@
+/* ==========================================
+ * TABLE: ATG_3859_OE_SLE_ORGN
+ * ========================================== */
+CREATE TABLE "ATG_3859_OE_SLE_ORGN"(
+ "ORDER_NO" CHAR(7) NOT NULL ,
+ "ORIGIN_ID" INTEGER,
+ "ORIGIN_DESC" CHAR(30));
